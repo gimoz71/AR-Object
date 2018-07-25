47ff4357273558229572eb05018dfeecd56f94b4
Gianluca Monti <gm3871@gmail.com>
2018-07-25 12:12:28 +0200

    show-hide object

---
+++ Assets/Scripts/ShowHIdeObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHIdeObject : MonoBehaviour {
+
+
+	public Object objectA;
+	public Object objectB;
+
+	public Button buttonA;
+	public Button buttonB;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
